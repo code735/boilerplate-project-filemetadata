480bf92fabae75e9fc434766c0c04f9b714691f5
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2024-03-18 16:58:05 +0530

    done

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' }); // Define the destination directory for uploaded files
 
-var app = express();
+const app = express();
 
-app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
 
+  const { originalname, mimetype, size } = req.file;
 
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
